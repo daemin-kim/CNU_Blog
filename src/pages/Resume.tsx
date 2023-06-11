a2270261b4da33bc865be095cf326cda4192173f
Kimdaemin <56399681+daemin-kim@users.noreply.github.com>
2023-06-11 21:47:11 +0900

    homework edit

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,18 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div>
+      나는 프로젝트 내역
+      <div>
+        <h3>CNU 공공데이터 경진대회 수상</h3>
+        <p>2023.04 ~ 2023.06</p>
+        <p>충남대학교 학생들을 위한 공모전 및 경진대회 아카이브 플랫폼 개발</p>
+
+        <h3>2022년 하계 모각코</h3>
+        <p>2022.07 ~ 2022.09</p>
+        <p>Flutter, Spring boot를 활용한 리마인더 어플 개발</p>
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
